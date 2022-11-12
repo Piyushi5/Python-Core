c0e93fef629fb2bda05109dfa104d0c461ce6849
Piyushi <109457316+Piyushi5@users.noreply.github.com>
2022-11-12 19:32:43 +0530

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. Write a program to calculate BMI by taking input from the user regarding his height and weight
+# 
+# formula = w/(h**2)
+
+# In[2]:
+
+
+# write a program to calculate the BMI
+
+w = float(input('enter your weight : ' ))
+h = float(input('enter your height :'))
+BMI = w/(h**2)
+print(BMI)
+
+
+# # .
+
+# In[ ]:
+
+
+
+
